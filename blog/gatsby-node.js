1f387d0603289102724fb30b437397d54df96275
ejaz khan <khaner@gmatc.matc.edu>
2021-02-17 19:55:24 -0600

    Completed Activity 3

---
+++ blog/gatsby-node.js
@@ -0,0 +1,35 @@
+const path = require(`path`);
+
+exports.createPages = ({ graphql , actions}) => {
+const { createPage } = actions;
+return new Promise((resolve, reject) => {
+graphql(`
+{
+    allContentfulBlogPost{
+        edges{
+            node{
+                id
+                slug
+            }
+        }
+    }
+}
+`).then(result => {
+    if(result.errors){
+        reject(result.errors);
+    }
+    result.data.allContentfulBlogPost.edges.forEach((edge)=> {
+        createPage({
+            path: edge.node.slug,
+            component: path.resolve(`./src/templates/blog-post.js`),
+            context:{
+                slug: edge.node.slug
+            }
+        })
+    })
+    resolve();
+})
+
+});
+
+};
